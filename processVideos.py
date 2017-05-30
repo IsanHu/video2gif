e97af24cd5b50f2442e15e23e08b14ab7e005f29
isan <3isan333@gmail.com>
2017-05-31 00:02:35 +0800

    no message

--- processVideos.py
+++ processVideos.py
@@ -225,33 +225,33 @@ def start_upload_audio_queue():
 def did_start_upload_audio_queue():
 	for file_name, video_path, audio_path in get_audio_path():
 		start = time.time()
-		# cmd = "java -jar %s 0 %s %s %s" % (config['XUNFEI_JAR'], config['XUNFEI_APPID'], config['XUNFEI_KEY'], zhuanyi(audio_path))
-		# print cmd
-		# try:
-		# 	result = json.loads(os.popen(cmd).read())
-		# except:
-		# 	# 上传失败,重新加入上传音频队列
-		# 	print "上传失败"
-		# 	uploadAudioQueue.put((file_name, video_path, audio_path))
-		# 	continue
+		cmd = "java -jar %s 0 %s %s %s" % (config['XUNFEI_JAR'], config['XUNFEI_APPID'], config['XUNFEI_KEY'], zhuanyi(audio_path))
+		print cmd
+		try:
+			result = json.loads(os.popen(cmd).read())
+		except:
+			# 上传失败,重新加入上传音频队列
+			print "上传失败"
+			uploadAudioQueue.put((file_name, video_path, audio_path))
+			continue
 
-		# print result
-		# if result['ok'] == 0:
-		# 	xunfei_id = result['data']
-		# 	info = videos[file_name]
-		# 	info['xunfei_id'] = xunfei_id
-		# else:
-		# 	# 上传失败,重新加入上传音频队列
-		# 	uploadAudioQueue.put((file_name, video_path, audio_path))
-		# 	continue
+		print result
+		if result['ok'] == 0:
+			xunfei_id = result['data']
+			info = videos[file_name]
+			info['xunfei_id'] = xunfei_id
+		else:
+			# 上传失败,重新加入上传音频队列
+			uploadAudioQueue.put((file_name, video_path, audio_path))
+			continue
 
 		# 临时
 		# xunfei_id = 'd2931698fdf8413f8b496de400025295'
 		# info = videos[file_name]
 		# info['xunfei_id'] = xunfei_id
 
-		# info['status'] = "生成字幕中"
-		# print audio_path
+		info['status'] = "生成字幕中"
+		print audio_path
 		print("上传音频用时: %.2fs" % (time.time() - start))
 
 def get_audio_path():
